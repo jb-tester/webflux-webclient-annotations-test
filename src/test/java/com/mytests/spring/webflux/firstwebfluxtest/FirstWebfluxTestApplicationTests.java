2bfcf90424d97019567d3597c41469a0c5fd45ef
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2019-11-05 22:40:50 +0300

    WebClientTest test

--- src/test/java/com/mytests/spring/webflux/firstwebfluxtest/FirstWebfluxTestApplicationTests.java
+++ src/test/java/com/mytests/spring/webflux/firstwebfluxtest/FirstWebfluxTestApplicationTests.java
@@ -1,13 +1,43 @@
 package com.mytests.spring.webflux.firstwebfluxtest;
 
+import com.mytests.spring.webflux.firstwebfluxtest.data.Person;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+//import static reactor.core.publisher.Mono.when;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(PersonMonoController.class)
 class FirstWebfluxTestApplicationTests {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    private PersonRepo personRepo;
 
     @Test
-    void contextLoads() {
+    public void testGetEmployeeById() {
+        Person p = new Person("test", 99, 99);
+        Mono<Person> employeeMono = Mono.just(p);
+
+        when(personRepo.getPersonById(99)).thenReturn(employeeMono);
+
+        webTestClient.get()
+                .uri("/persons/mono/{id}", 99)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Person.class)
+                .value(employee1 -> p.getAge(), equalTo(99));
     }
 
 }
