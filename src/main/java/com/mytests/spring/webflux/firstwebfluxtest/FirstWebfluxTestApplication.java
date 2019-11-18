b03479e6160272736c615adaf2b5deab650f6240
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-11-18 13:41:48 +0300

    debug tests

--- src/main/java/com/mytests/spring/webflux/firstwebfluxtest/FirstWebfluxTestApplication.java
+++ src/main/java/com/mytests/spring/webflux/firstwebfluxtest/FirstWebfluxTestApplication.java
@@ -2,11 +2,13 @@ package com.mytests.spring.webflux.firstwebfluxtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class FirstWebfluxTestApplication {
 
     public static void main(String[] args) {
+        //Hooks.onOperatorDebug();
         SpringApplication.run(FirstWebfluxTestApplication.class, args);
         PersonWebClient webClient = new PersonWebClient();
         webClient.consume();
